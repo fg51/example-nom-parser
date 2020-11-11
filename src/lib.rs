ce10a98e4c82cf7d5c065bcb4106263e7ec2883d
kflange <zknsftk1msrk@gmail.com>
2020-11-11 19:42:27 +0900

    Added: sample

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::combinator::all_consuming;
+use nom::combinator::value;
+use nom::{error::ErrorKind as NomErrorKind, IResult};
+
 mod errors;
 
-fn add() -> u32 {
-    return 1;
+#[derive(Clone, Debug, PartialEq)]
+enum JsonBool {
+    True,
+    False,
+}
+
+fn json_bool(s: &str) -> IResult<&str, JsonBool> {
+    alt((
+        value(JsonBool::True, tag("true")),
+        value(JsonBool::False, tag("false")),
+    ))(s)
+}
+
+fn parse_sample(s: &str) -> errors::Result<JsonBool> {
+    let (s, result) = all_consuming(json_bool)(s).map_err(|nom_err| match nom_err {
+        nom::Err::Incomplete(_) => unreachable!(),
+        nom::Err::Error(e) => todo!(),
+        nom::Err::Failure(e) => todo!(),
+    })?;
+    return Ok(result);
 }
 
 #[cfg(test)]
@@ -9,7 +33,17 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_one() {
-        assert_eq!(add(), 1);
+    fn test_json_bool() {
+        assert_eq!(json_bool("false"), Ok(("", JsonBool::False)));
+        assert_eq!(json_bool("true"), Ok(("", JsonBool::True)));
+        assert!(json_bool("foo").is_err());
+    }
+
+    #[test]
+    fn test_parse_sample() {
+        match parse_sample("false") {
+            Ok(_) => assert!(true),
+            Err(e) => assert!(false, format!("{:?}", e)),
+        }
     }
 }
